543abc441dc5e39b2a5c641ddff854d6af0c17b2
Cassoli <fernandes.cassoli@gmail.com>
2024-02-07 09:34:04 -0300

    first commit

---
+++ src/main/java/mypact/veor03/Veor03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mypact.veor03;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class Veor03 {
+
+    public static void main(String[] args) {
+        double v[] ={3.5,2.75,9,-4.5};
+        for(double valor:v ){
+            System.out.println(valor+" ");
+        }
+    }
+}
